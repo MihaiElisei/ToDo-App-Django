6636e8fcd6158172dfd980d20aee9d512c4bb61b
MihaiElisei <mihai.m.elisei@gmail.com>
2022-12-06 12:55:28 +0000

    add view for add new item and create html form

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    # query set for all items in the model
+    items = Item.objects.all()
+    # a dictionary with all items 
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    return render(request, 'todo/add_item.html')
