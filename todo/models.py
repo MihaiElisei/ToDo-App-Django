808401bdc719857f5f2b480d542a78c8ca33e21f
MihaiElisei <mihai.m.elisei@gmail.com>
2022-12-06 12:32:24 +0000

    register Item Model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    # create Item model table
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    # return the name of the Items in table
+    def __str__(self):
+        return self.name
